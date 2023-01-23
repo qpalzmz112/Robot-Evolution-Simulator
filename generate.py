ccbc12bc7e2d2473dfb5bdad3719719d74402121
Sean Carlson <seancarlson112@gmail.com>
2023-01-23 12:26:21 -0600

    done with task J

--- generate.py
+++ generate.py
@@ -11,11 +11,11 @@ def Create_Robot():
 def Generate_Body():
 	pyrosim.Start_URDF("body.urdf")
 
-	pyrosim.Send_Cube(name="BackLeg", pos=[0,0,0.5] , size=[1, 1, 1])
-	pyrosim.Send_Joint( name = "BackLeg_Torso" , parent= "BackLeg", child = "Torso" , type = "revolute", position = [0.5,0,1])
-	pyrosim.Send_Cube(name="Torso", pos=[0.5,0,0.5] , size=[1, 1, 1])
-	pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [1,0,0])
-	pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5] , size=[1, 1, 1])
+	pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[1, 1, 1])
+	pyrosim.Send_Joint(name = "Torso_BackLeg", parent = "Torso", child = "BackLeg", type = "revolute", position = [1, 0, 1])
+	pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
+	pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type = "revolute", position = [2, 0, 1])
+	pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size = [1, 1, 1])
 
 	pyrosim.End()
 
@@ -24,8 +24,12 @@ def Generate_Brain():
 	pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
 	pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
 	pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
-	pyrosim.Send_Motor_Neuron(name = 3 , jointName = "BackLeg_Torso")
+	pyrosim.Send_Motor_Neuron(name = 3 , jointName = "Torso_BackLeg")
 	pyrosim.Send_Motor_Neuron(name = 4 , jointName = "Torso_FrontLeg")
+	pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = -1.0 )
+	pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = -1.0 )
+	pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = -1.0 )
+	pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 4 , weight = -1.0 )
 	pyrosim.End()
 
 Create_World()
